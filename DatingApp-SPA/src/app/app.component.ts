a73518641744fdfa8a260eb256eacffb782af5de
chinhyanglong <toilathor@gmail.com>
2019-06-07 16:31:25 +0700

    End of user interface

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -8,7 +8,7 @@ import {JwtHelperService} from '@auth0/angular-jwt';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   jwtHelper = new JwtHelperService();
   constructor(private authServices: AuthService) {}
   ngOnInit() {
